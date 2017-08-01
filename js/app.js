9f064a3698531dd4caf5835f9a0c128e07672638
atan009 <atan009@users.noreply.github.com>
2017-08-01 17:18:56 -0400

    list is filtering now

--- js/app.js
+++ js/app.js
@@ -23,11 +23,31 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-var fullList = ko.observableArray([
-    { loc: "New York City, NY", latitude: "40.7128", longitude: "-74.0059" },
-    { loc: "San Francisco, CA", latitude: "37.7749", longitude: "-122.4194" },
-    { loc: "Las Vegas, NV", latitude: "36.1699", longitude: "-115.1398" },
-    { loc: "Chicago, IL", latitude: "41.8781", longitude: "-87.6298" },
-    { loc: "Los Angeles, CA", latitude: "34.0522", longitude: "-118.2437" }
-]);
-ko.applyBindings(fullList);
+$(function() {
+	var locations = [
+			{ name: "New York City, NY", latitude: "40.7128", longitude: "-74.0059" },
+		    { name: "San Francisco, CA", latitude: "37.7749", longitude: "-122.4194" },
+		    { name: "Las Vegas, NV", latitude: "36.1699", longitude: "-115.1398" },
+		    { name: "Chicago, IL", latitude: "41.8781", longitude: "-87.6298" },
+		    { name: "Los Angeles, CA", latitude: "34.0522", longitude: "-118.2437" }
+		];
+
+		var viewModel = {
+			locations: ko.observableArray(locations),
+
+			query: ko.observable(''),
+
+			search: function(value) {
+			viewModel.locations.removeAll();
+				for(var x in locations) {
+		  			if(locations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+		    		viewModel.locations.push(locations[x]);
+		  			}
+				}
+			}
+		};
+
+	viewModel.query.subscribe(viewModel.search);
+
+	ko.applyBindings(viewModel);
+});
