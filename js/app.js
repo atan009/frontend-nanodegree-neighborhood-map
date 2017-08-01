dafa626d55496b33c873147fd1bc696e60c12d18
atan009 <atan009@users.noreply.github.com>
2017-08-01 15:33:58 -0400

    knockout running properly

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-function openNav() {
-	document.getElementById("mySidenav").style.width = "25%";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
 var map;
 var center;
     function initMap() {
@@ -21,3 +13,21 @@ var center;
 	});
 }
 
+//opens Filter Menu to 25%
+function openNav() {
+	document.getElementById("mySidenav").style.width = "25%";
+}
+
+//closes Filter Menu
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+var fullList = ko.observableArray([
+    { loc: "New York City, NY", latitude: "40.7128", longitude: "-74.0059" },
+    { loc: "San Francisco, CA", latitude: "37.7749", longitude: "-122.4194" },
+    { loc: "Las Vegas, NV", latitude: "36.1699", longitude: "-115.1398" },
+    { loc: "Chicago, IL", latitude: "41.8781", longitude: "-87.6298" },
+    { loc: "Los Angeles, CA", latitude: "34.0522", longitude: "-118.2437" }
+]);
+ko.applyBindings(fullList);
