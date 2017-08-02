8fcc3d478d19fc90b0008c959d72bd9dd908b845
atan009 <atan009@users.noreply.github.com>
2017-08-02 11:31:14 -0400

    Pins loaded, mouse over, mouse out, click, close box

--- js/app.js
+++ js/app.js
@@ -24,28 +24,120 @@ function closeNav() {
 }
 
 $(function() {
-	var locations = [
-			{ name: "New York City, NY", latitude: "40.7128", longitude: "-74.0059" },
-		    { name: "San Francisco, CA", latitude: "37.7749", longitude: "-122.4194" },
-		    { name: "Las Vegas, NV", latitude: "36.1699", longitude: "-115.1398" },
-		    { name: "Chicago, IL", latitude: "41.8781", longitude: "-87.6298" },
-		    { name: "Los Angeles, CA", latitude: "34.0522", longitude: "-118.2437" }
-		];
+	// This function takes in a COLOR, and then creates a new marker
+    // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+    // of 0, 0 and be anchored at 10, 34).
+    function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+            '|40|_|%E2%80%A2',
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21,34));
+        return markerImage;
+    }
 
-		var viewModel = {
-			locations: ko.observableArray(locations),
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+    var defaultIcon = makeMarkerIcon('FE7569');
 
-			query: ko.observable(''),
+	//array with all the information
+	var topCities = [
+		{ "name": "New York City, NY",
+			"loc": { lat: 40.7128, lng: -74.0059 }
+		},
+	    { "name": "San Francisco, CA",
+	    	"loc": { lat: 37.7749, lng: -122.4194 }
+	    },
+	    { "name": "Las Vegas, NV",
+	    	"loc": { lat: 36.1699, lng: -115.1398 }
+	    },
+	    { "name": "Chicago, IL",
+	    	"loc": { lat: 41.8781, lng: -87.6298 }
+	    },
+	    { "name": "Los Angeles, CA",
+	    	"loc": { lat: 34.0522, lng: -118.2437 }
+	    }
+	];
 
-			search: function(value) {
-			viewModel.locations.removeAll();
-				for(var x in locations) {
-		  			if(locations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-		    		viewModel.locations.push(locations[x]);
-		  			}
-				}
+	var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+
+	var markers = [];
+	for (var i = 0; i < topCities.length; i++) {
+		var marker = new google.maps.Marker({
+            position: topCities[i].loc,
+            map: map,
+            title: topCities[i].name,
+            animation: google.maps.Animation.DROP,
+            id: i,
+            icon: defaultIcon
+        });
+        
+
+        marker.addListener('click', function() {
+        	if (marker.getAnimation() === null) {
+            	this.setAnimation(google.maps.Animation.BOUNCE);
+            }
+            populateInfoWindow(this, largeInfowindow);
+
+        });
+
+        marker.addListener('mouseover', function() {
+        	this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        });
+
+        markers.push(marker);
+	}
+
+	function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '</div>');
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick',function(){
+            	infowindow.setMarker = null;
+            	marker.setAnimation(null);
+            });
+        }
+    }
+
+	//filterList display
+	var viewModel = {
+		locations: ko.observableArray(topCities),
+
+		query: ko.observable(''),
+
+		search: function(value) {
+			for (var i = 0; i < markers.length; i++) {
+				markers[i].setMap(null);
 			}
-		};
+			markers = [];
+
+			viewModel.locations.removeAll();
+			for(var x in topCities) {
+	  			if(topCities[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+	    			viewModel.locations.push(topCities[x]);
+	    			markers.push(topCities[x]);
+	  			}
+			}
+
+			
+		}
+
+	};
+
+	//marker display
+	var viewMarker = {
+		display: console.log(viewModel.locations())
+	};
 
 	viewModel.query.subscribe(viewModel.search);
 
